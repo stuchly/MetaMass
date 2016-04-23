25623e92dcd6e322a55847c618f1559500a7aa5f
stuchly <grigorij@seznam.cz>
2016-04-24 01:37:16 +0200

    v0.0.1

--- inst/doc/MetaMass.R
+++ inst/doc/MetaMass.R
@@ -12,7 +12,7 @@
 
 
 ###################################################
-### code chunk number 2: MetaMass.rnw:81-84
+### code chunk number 2: MetaMass.rnw:80-83
 ###################################################
 library(MetaMass)
 data(AnnotationAM)
@@ -20,44 +20,44 @@ head(AnnotationAM)
 
 
 ###################################################
-### code chunk number 3: MetaMass.rnw:93-94 (eval = FALSE)
+### code chunk number 3: MetaMass.rnw:92-93 (eval = FALSE)
 ###################################################
 ## data_table<-read.table(filename,header=TRUE,sep="\t")
 
 
 ###################################################
-### code chunk number 4: MetaMass.rnw:106-107 (eval = FALSE)
+### code chunk number 4: MetaMass.rnw:105-106 (eval = FALSE)
 ###################################################
 ## colnames(data_table)[sapply(data.table,is.numeric)]
 
 
 ###################################################
-### code chunk number 5: MetaMass.rnw:138-140
+### code chunk number 5: MetaMass.rnw:137-139
 ###################################################
 filename<-system.file("extdata","Bileck.txt",package="MetaMass")
 filename
 
 
 ###################################################
-### code chunk number 6: MetaMass.rnw:149-150 (eval = FALSE)
+### code chunk number 6: MetaMass.rnw:148-149 (eval = FALSE)
 ###################################################
 ## Res1= analyze.MSfile(MSfile="filename.txt", Metadata= "Christoforou", output="myfile2")
 
 
 ###################################################
-### code chunk number 7: MetaMass.rnw:161-162 (eval = FALSE)
+### code chunk number 7: MetaMass.rnw:160-161 (eval = FALSE)
 ###################################################
 ## Res2= analyze.MSfile(MSfile="filename.txt", Metadata= "Christoforou", output="myfile2",markers=c(3:7))
 
 
 ###################################################
-### code chunk number 8: MetaMass.rnw:171-172 (eval = FALSE)
+### code chunk number 8: MetaMass.rnw:170-171 (eval = FALSE)
 ###################################################
 ## ?AnnotationAM
 
 
 ###################################################
-### code chunk number 9: MetaMass.rnw:180-181 (eval = FALSE)
+### code chunk number 9: MetaMass.rnw:179-180 (eval = FALSE)
 ###################################################
 ## Res3= analyze.MSfile(MSfile="filename.txt", Metadata= "Christoforou", output="myfile3",group=0,cluster.metadata=TRUE)
 
@@ -69,7 +69,7 @@ filename
 
 
 ###################################################
-### code chunk number 11: MetaMass.rnw:209-216
+### code chunk number 11: MetaMass.rnw:208-215
 ###################################################
 file2<-system.file("extdata","Data_Fig_1b.txt",package="MetaMass")
 
@@ -81,19 +81,19 @@ head(cls2_1)
 
 
 ###################################################
-### code chunk number 12: MetaMass.rnw:226-227
+### code chunk number 12: MetaMass.rnw:225-226
 ###################################################
 data2<-data.frame(data2,main_component1=cls2_1$main_component[data2$cluster])
 
 
 ###################################################
-### code chunk number 13: MetaMass.rnw:243-244
+### code chunk number 13: MetaMass.rnw:241-242
 ###################################################
 file1<-system.file("extdata","Data_Fig_1a.txt",package="MetaMass")
 
 
 ###################################################
-### code chunk number 14: MetaMass.rnw:255-258
+### code chunk number 14: MetaMass.rnw:252-255
 ###################################################
 ##proteins identified by gene-name -> annotation.ID=2 (see ?AnnotationAM)
 ##cluster with respect metadata only (group=0)
@@ -101,7 +101,7 @@ res1<-analyze.MSfile(MSfile=file1,Metadata=c("Christoforou"),output="res1",group
 
 
 ###################################################
-### code chunk number 15: MetaMass.rnw:276-280
+### code chunk number 15: MetaMass.rnw:273-277
 ###################################################
 file2<-system.file("extdata","Data_Fig_1b.txt",package="MetaMass")
 
@@ -110,7 +110,7 @@ res2<-analyze.MSfile(MSfile=file2,Metadata=c("Christoforou"),output="res2")
 
 
 ###################################################
-### code chunk number 16: MetaMass.rnw:287-292
+### code chunk number 16: MetaMass.rnw:284-289
 ###################################################
 ##compare multiple files component fractionation with Metadata
 files1<-system.file("extdata",c("Bileck.txt","Thakar.txt","Carvalho.txt"),package="MetaMass")
@@ -120,16 +120,16 @@ res3<-analyze.MSfile(MSfile=files1,Metadata=c("Christoforou"),output="res3")
 
 
 ###################################################
-### code chunk number 17: MetaMass.rnw:301-302
+### code chunk number 17: MetaMass.rnw:298-299
 ###################################################
 res4<-analyze.MSfile(MSfile=file2,Metadata=c("Christoforou"),output="res2_4annot",clusters=480,markers=c(3,4,6,7))
 
 
 ###################################################
-### code chunk number 18: MetaMass.rnw:308-310 (eval = FALSE)
+### code chunk number 18: MetaMass.rnw:305-307 (eval = FALSE)
 ###################################################
 ## par(mfrow=c(5,3),mar=c(1, 4, 2.2, 1) + 0.1)
-## plot.prAM(res4) #plot in  6 rows and 3 columns
+## plot.prAM(res4) #plot in  5 rows and 3 columns
 
 
 ###################################################
@@ -137,7 +137,7 @@ res4<-analyze.MSfile(MSfile=file2,Metadata=c("Christoforou"),output="res2_4annot
 ###################################################
 pdf('roc1.pdf')
 par(mfrow=c(5,3),mar=c(1, 4, 2.2, 1) + 0.1,cex=0.7)
-plot.prAM(res4) #plot in 5 rows and 4 columns    # Produces Figure \ref{fig1}
+plot.prAM(res4) #plot in 5 rows and 3 columns    # Produces Figure \ref{fig1}
 dev.off()
 
 
